3d69925fe078c534cdb59d6d04f17b6d21a1c6b3
Alan Weik <alaan.m@hotmail.com>
2022-01-04 14:24:49 +0100

    Add project files.

---
+++ FreakyFashionServices.CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,45 @@
+﻿using FreakyFashionServices.CatalogService.Models.Domain;
+using FreakyFashionServices.CatalogService.Models.DTO;
+using FreakyFashionServices.CatalogService.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreakyFashionServices.CatalogService.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private ApplicationContext Context { get; set; }
+
+        public CatalogController(ApplicationContext context)
+        {
+            Context = context;
+        }
+
+        // POST api/products
+        [HttpPost]
+        public IActionResult RegisterProduct(ProductDto productDto)
+        {
+
+            var name = productDto.Name;
+            name = name.Replace("-", "");
+            name = name.Replace(" ", "-");
+
+
+            var product = new Product(
+                id: productDto.Id,
+                name: productDto.Name,
+                description: productDto.Description,
+                imageUrl: productDto.ImageUrl,
+                price: productDto.Price,
+                articleNumber: productDto.ArticleNumber,
+                urlSlug: name.ToLower()
+                );
+
+            Context.Product.Add(product);
+            Context.SaveChanges(); 
+
+            return Created("", productDto); // 201 Created
+        }
+    }
+}
---
+++ FreakyFashionServices.CatalogService/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using FreakyFashionServices.CatalogService.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashionServices.CatalogService.Data
+{
+    public class ApplicationContext : DbContext 
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+}
